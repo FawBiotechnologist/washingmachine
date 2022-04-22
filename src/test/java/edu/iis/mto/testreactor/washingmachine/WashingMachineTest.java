99456d65d4f95598b61a8d00a9233a0848624003
Lukasz Krulik <228780@edu.p.lodz.pl>
2022-04-22 18:39:23 +0200

    added proper batch test

--- src/test/java/edu/iis/mto/testreactor/washingmachine/WashingMachineTest.java
+++ src/test/java/edu/iis/mto/testreactor/washingmachine/WashingMachineTest.java
@@ -1,12 +1,12 @@
 package edu.iis.mto.testreactor.washingmachine;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class WashingMachineTest {
@@ -24,9 +24,24 @@ class WashingMachineTest {
         washingMashine = new WashingMachine(dirtDetector, engine, waterPump);
     }
 
+    //###TestyStanu###
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void properBatchJeansTypeFiveKgShortProgramWithSpinExpectingSuccess() {
+        LaundryBatch laundryBatch = LaundryBatch.builder()
+                .withMaterialType(Material.JEANS)
+                .withWeightKg(2)
+                .build();
+        ProgramConfiguration programConfiguration = ProgramConfiguration.builder()
+                .withProgram(Program.SHORT)
+                .withSpin(true)
+                .build();
+        LaundryStatus result = washingMashine.start(laundryBatch, programConfiguration);
+        LaundryStatus expectedResult = LaundryStatus.builder()
+                .withResult(Result.SUCCESS)
+                .withErrorCode(ErrorCode.NO_ERROR)
+                .withRunnedProgram(Program.SHORT)
+                .build();
+        assertEquals(expectedResult, result);
     }
 
 }
