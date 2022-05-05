f23f0f55631706a2201bee646bce1d3ee13ccdf4
Lukasz Krulik <228780@edu.p.lodz.pl>
2022-05-05 20:05:09 +0200

    covering methods with tests

--- src/test/java/edu/iis/mto/testreactor/washingmachine/WashingMachineTest.java
+++ src/test/java/edu/iis/mto/testreactor/washingmachine/WashingMachineTest.java
@@ -7,6 +7,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class WashingMachineTest {
@@ -19,21 +21,33 @@ class WashingMachineTest {
     private WaterPump waterPump;
     private WashingMachine washingMachine;
     private LaundryBatch properLaundryBatch;
+    private LaundryBatch overweightLaundryBatch;
     private Program nonAutoDetectProgram;
-    private Material irrelevantMaterialType;
+    private Material materialTypeJeans;
+    private Material materialTypeCotton;
     private double anyProperWeightInKg;
+    private double anyOverweightInKg;
     private ProgramConfiguration nonAutoDetectProgramConfigurationWithSpin;
+    private ProgramConfiguration autodetectProgramConfigurationWithSpin;
     private LaundryStatus successWithNoErrors;
+    private LaundryStatus failureTooHeavy;
+    private static final Percentage AVERAGE_DEGREE = new Percentage(50.0d);
+    private static final Percentage ABOVE_AVERAGE_DEGREE = new Percentage(50.1d);
 
     @BeforeEach
     void setUp() throws Exception {
         washingMachine = new WashingMachine(dirtDetector, engine, waterPump);
         anyProperWeightInKg = 2d;
-        irrelevantMaterialType = Material.JEANS;
+        materialTypeJeans = Material.JEANS;
+        materialTypeCotton = Material.COTTON;
         nonAutoDetectProgram = Program.SHORT;
         properLaundryBatch = properLaundryBatchCreator();
         nonAutoDetectProgramConfigurationWithSpin = getNonAutoDetectProgramConfiguration(true);
         successWithNoErrors = statusSuccessWithNoErrors(nonAutoDetectProgram);
+        anyOverweightInKg = 10d;
+        overweightLaundryBatch = overweightLaundryBatchCreator();
+        failureTooHeavy = statusFailureTooHeavy();
+        autodetectProgramConfigurationWithSpin = getAutodetectProgramConfiguration(true);
     }
 
     //###TestyStanu###
@@ -44,11 +58,43 @@ class WashingMachineTest {
         assertEquals(successWithNoErrors, result);
     }
 
+    @Test
+    void overweightBatchCottonTypeTenKgShortProgramWithSpinExpectingStatusError() {
+        LaundryStatus result = washingMachine.start(overweightLaundryBatch, nonAutoDetectProgramConfigurationWithSpin);
+
+        assertEquals(failureTooHeavy, result);
+    }
+
+    @Test
+    void properBatchJeansTypeFiveKgAutodetectAverageDirtDegreeWithSpinExpectingSuccess() {
+        when(dirtDetector.detectDirtDegree(any())).thenReturn(AVERAGE_DEGREE);
+
+        LaundryStatus result = washingMachine.start(properLaundryBatch, autodetectProgramConfigurationWithSpin);
+
+        assertEquals(statusSuccessWithNoErrors(Program.MEDIUM), result);
+    }
+
+    @Test
+    void properBatchJeansTypeFiveKgAutodetectAboveAverageDirtDegreeWithSpinExpectingSuccess() {
+        when(dirtDetector.detectDirtDegree(any())).thenReturn(ABOVE_AVERAGE_DEGREE);
+
+        LaundryStatus result = washingMachine.start(properLaundryBatch, autodetectProgramConfigurationWithSpin);
+
+        assertEquals(statusSuccessWithNoErrors(Program.LONG), result);
+    }
+
     private LaundryStatus statusSuccessWithNoErrors(Program programType) {
         return LaundryStatus.builder()
                 .withResult(Result.SUCCESS)
                 .withErrorCode(ErrorCode.NO_ERROR)
                 .withRunnedProgram(programType)
+                .build();
+    }
+    private LaundryStatus statusFailureTooHeavy() {
+        return LaundryStatus.builder()
+                .withResult(Result.FAILURE)
+                .withErrorCode(ErrorCode.TOO_HEAVY)
+                .withRunnedProgram(null)
                 .build();
     }
 
@@ -59,10 +105,24 @@ class WashingMachineTest {
                 .build();
     }
 
+    private ProgramConfiguration getAutodetectProgramConfiguration(boolean spin) {
+        return ProgramConfiguration.builder()
+                .withProgram(Program.AUTODETECT)
+                .withSpin(spin)
+                .build();
+    }
+
     private LaundryBatch properLaundryBatchCreator() {
         return LaundryBatch.builder()
-                .withMaterialType(irrelevantMaterialType)
+                .withMaterialType(materialTypeJeans)
                 .withWeightKg(anyProperWeightInKg)
+                .build();
+    }
+
+    private LaundryBatch overweightLaundryBatchCreator() {
+        return LaundryBatch.builder()
+                .withMaterialType(materialTypeCotton)
+                .withWeightKg(anyOverweightInKg)
                 .build();
     }
 
